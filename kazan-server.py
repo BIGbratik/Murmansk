7015ad122655b34ecb9d0deead041593ef14a973
Костя Ливчак <prayDEATH@mail.ru>
2022-05-04 11:49:34 +0300

    first commit

---
+++ kazan-server.py
@@ -0,0 +1,107 @@
+from flask import Flask, Response, render_template, request, make_response
+from flask_cors import CORS
+import sys, json
+
+app = Flask(__name__)
+CORS(app)
+
+def getData(name):
+   file1=open("./data/"+name+".txt","r",encoding='utf-8')
+   data_res=[]
+   while True:
+      line=file1.readline()
+      if not line:
+         break
+      else:
+         split_line=line.split('|')
+         if len(split_line)==4:
+            data_res.append({"Coordinate1":split_line[0], 
+            "Coordinate2":split_line[1],
+            "Name":split_line[2],
+            "Extra":split_line[3]})
+         else:
+            data_res.append({"Coordinate1":split_line[0], 
+            "Coordinate2":split_line[1],
+            "Name":split_line[2],
+            "Extra":""})
+   file1.close()
+   return data_res
+
+def checkKey(key):
+   file1=open("./data/keys.txt","r",encoding='utf-8')
+   res=0
+   while True:
+      line=file1.readline()
+      if not line:
+         break
+      else:
+         if line==key:
+            res=1
+   return res
+
+def addObject(data):
+   file=open("./data/"+data["File"]+".txt","a",encoding='utf-8')
+   if data["Extra"]!="":
+      file.write(data["Coordinate1"]+"|"+data["Coordinate2"]+"|"+data["Name"]+"|"+data["Extra"]+"\n")
+   else:
+      file.write(data["Coordinate1"]+"|"+data["Coordinate2"]+"|"+data["Name"]+"\n")
+
+@app.route('/')
+def index():
+   return render_template('index.html')
+
+@app.route('/bioSocCS')
+def bio():
+   return render_template('bioSocCS.html')
+
+@app.route('/harkPOO')
+def hark():
+   return render_template('harkPOO.html')
+
+@app.route('/istochnikCS')
+def ist():
+   return render_template('istochnikCS.html')
+
+@app.route('/obshCS')
+def obsh():
+   return render_template('obshCS.html')
+   
+@app.route('/orgTec')
+def org():
+   return render_template('orgTec.html')
+
+@app.route('/pokazateli')
+def pokaz():
+   return render_template('pokazateli.html')
+
+@app.route('/pooCS')
+def poo():
+   return render_template('pooCS.html')
+
+@app.route('/riskNatCS')
+def risk():
+   return render_template('riskNatCS.html')
+
+@app.route('/objects',methods=['GET'])
+def search():
+    query = request.args.get('query')
+    if query and query != '':
+        return json.dumps(getData(query))
+    else:
+        return json.dumps('Не введен запрос!')
+
+@app.route('/auth', methods=['POST'])
+def key():
+   key=request.get_json(force=False, silent=False, cache=False)
+   return json.dumps(checkKey(key))
+
+@app.route('/add', methods=['POST'])
+def add():
+   add_data=request.get_json(force=False, silent=False, cache=False)
+   addObject(add_data)
+   return make_response('Данные успешно записаны', 200)
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
+    
---
+++ static/javascript/input.js
@@ -0,0 +1,33 @@
+
+async function accessCtrl()
+{
+
+    var accKey=document.getElementById("Key").value;
+    document.getElementById("Key").value="";
+
+    let response = await fetch(`http://localhost:5000/auth`,
+    {
+        method: 'POST',
+        headers: 
+        {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(accKey)
+    });
+
+    if (response.ok)
+    {
+        let res=await response.json();
+        if (res==0)
+        {
+            alert("Введённый ключ не действителен!!!");
+        }
+        else if (res==1)
+        {
+            document.getElementById('functional-container').style.display='block';
+            document.getElementById('input-container').style.display='none';
+        }
+    }
+
+
+}
